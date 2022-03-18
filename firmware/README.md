5e4a4b5f82181cc279b1a0b8d2b9a37a20265f1e
beaumont <Wim Beaumont>
2022-03-18 11:54:40 +0100

    firmware : added digpr4 as source directory , moved all src files for digtpr1 to src  updated .ignore files
    Signed-off-by: beaumont <Wim Beaumont>

--- firmware/README.md
+++ firmware/README.md
@@ -6,5 +6,12 @@ So changes in this repository will be overwriten.
 
 The *.sch are schematic files that are generated (  and can be "read"   by ISE 14.7 
 
-exception is digpr2 ,pract1, SiPm  these  are  the design directory  see README.md in that directory 
+exception is digpr4 , digpr2 ,pract1, SiPm  these  are  the design directory  see README.md in that directory 
+
+history
+mrt 2022  make subdir src for digpr1 and digpr 4 so more clear what are generated files ( not on git ) and what are the source files (on git) to regenerate the project
+When the project is first time generated the ip cores have to be re-generated also
+digpr4 is converting to the digpr4 diff version. no diff output yet
+
+
 
--- firmware/digpr1/src/LUT4_4.vhd
+++ firmware/digpr1/src/LUT4_4.vhd
@@ -39,23 +39,22 @@ architecture Behavioral of LUT4_4 is
 begin
 
 LUT  : with  LIN select 
-	  Dout <= "0001" when "0000",
-                                "0011" when "0001",
-                                "0010" when "0011",
-                                "0110" when "0010",
-                                "0111" when "0110",
-                                "0101" when "0111",
-                                "0100" when "0101",
-                                "1100" when "0100",
-                                "1101" when "1100",
-                                "1111" when "1101",
-                                "1110" when "1111",
-                                "1010" when "1110",
-                                "1011" when "1010",
-                                "1001" when "1011",
-                                "1000" when "1001",
-                           "0000" when others;
-
+	Dout <=	"0001" when "0000",
+				"0010" when "0001",
+				"0011" when "0010",
+				"0100" when "0011",
+				"0101" when "0100",
+				"0110" when "0101",
+				"0111" when "0110",
+				"1000" when "0111",
+				"1001" when "1000",
+				"1010" when "1001",
+				"1011" when "1010",
+				"1100" when "1011",
+				"1101" when "1100",
+				"1110" when "1101",
+				"1111" when "1110",
+			   "0000" when others;
 
 end Behavioral;
 

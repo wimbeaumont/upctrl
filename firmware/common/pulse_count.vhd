28053df045687da76db98502927ab0cc2a98c132
beaumont <Wim Beaumont>
2023-03-14 18:51:59 +0100

    moved common components to 1 directory

---
+++ firmware/common/pulse_count.vhd
@@ -0,0 +1,95 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    16:21:49 03/18/2022 
+-- Design Name: 
+-- Module Name:    pulse_count - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description:  
+--    up counter with two enable inputs ( both to be high) 
+--    with pulse detection input ( to FD in series) 
+--    two clear input  
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+library UNISIM;
+use UNISIM.VComponents.all;
+
+entity pulse_count is
+	generic ( CWIDTH : integer :=16 );
+   Port ( D : in  STD_LOGIC; -- pulse input
+           EnA : in  STD_LOGIC;
+           EnB : in  STD_LOGIC;
+           CLR : in  STD_LOGIC;
+           CLK : in  STD_LOGIC; --sync clk iput
+			  CNTE_out : out STD_LOGIC; -- count enable status
+			  Pout: out STD_LOGIC; --  pulse detect output 1 clock high
+           Q : out  STD_LOGIC_VECTOR (CWIDTH-1 downto 0));
+end pulse_count;
+
+architecture Behavioral of pulse_count is
+
+   component FD
+      generic( INIT : bit :=  '0');
+      port ( C : in    std_logic; 
+             D : in    std_logic; 
+             Q : out   std_logic);
+   end component;
+   attribute BOX_TYPE of FD : component is "BLACK_BOX";
+
+
+	signal CNT1_CE , Qff1_2,Qff1_1,Qff1_3: std_logic;
+
+begin
+
+
+ff1_1 : FD
+      port map (C=>CLK,
+                D=>D,
+                Q=>Qff1_1);
+   
+ff1_2 : FD
+      port map (C=>CLK,
+                D=>Qff1_1,
+                Q=>Qff1_2);
+					 
+
+ff1_3 : FD  -- cp[[ied from digpr4diff, not so clear why needed
+      port map (C=>CLK,
+                D=>Qff1_2,
+                Q=>Qff1_3);
+
+Pout <= (not  Qff1_3 )  and  (Qff1_1 and Qff1_2);					 
+					 					 
+					 
+CNT1_CE <= (not  Qff1_2 )  and  (Qff1_1 and  EnA and EnB );
+CNTE_out <= CNT1_CE;
+
+count1  : entity work.counter
+      generic map ( CWIDTH => CWIDTH)
+      port map (C=>CLK,
+                CE=>CNT1_CE,
+                CLR=>CLR,
+                Q=>Q
+              );
+
+
+
+end Behavioral;
+
---
+++ firmware/common/timewindow.vhd
@@ -0,0 +1,70 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    14:37:00 02/28/2020 
+-- Design Name: 
+-- Module Name:    timewindow - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity timewindow is
+    Port ( clk : in  STD_LOGIC;
+           rst : in  STD_LOGIC;
+           TwHigh : in  STD_LOGIC_VECTOR (7 downto 0);
+           TwLow : in  STD_LOGIC_VECTOR (7 downto 0);
+           start : in  STD_LOGIC;
+           WinActive : out  STD_LOGIC);
+end timewindow;
+
+architecture Behavioral of timewindow is
+constant CNT_LENGTH : integer := 28;
+signal Dset : STD_LOGIC_VECTOR (CNT_LENGTH-1 downto 0);
+signal WinActiveTime :STD_LOGIC;
+
+begin
+
+Dset( CNT_LENGTH-17 downto 0) <= (others => '1' );
+Dset (CNT_LENGTH-9  downto CNT_LENGTH-16)  <=  TwLow;
+Dset (CNT_LENGTH-1 downto CNT_LENGTH-8)  <=  TwHigh;
+
+windowcnt1 : entity work.gen_down_cnt 
+			generic map ( 
+			 C_WIDTH =>  Dset'LENGTH,
+			 CNT_EN => 0,  -- stop at 0 ignore  cnt input 
+			 LOAD_EN => 1,
+			 LRSTLVL => '1' )
+			port map (
+				rst => rst,
+				d => Dset,
+				zero_bar => WinActiveTime,
+				q=> open,
+				load => start,
+				clk  => clk,
+				one_bar => open
+			);
+
+WinActive <= WinActiveTime OR start; -- if start is high the window is always active !! 
+end Behavioral;
+
